0a84679e956ee03e18e55e8267c1b2d22f6355a9
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-02 21:14:10 +0200

    PruebaPython

---
+++ bot.py
@@ -0,0 +1,33 @@
+# bot.py
+import requests  
+import os
+from flask import Flask, request
+# Add your telegram token as environment variable
+BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def main():  
+    data = request.json
+
+    print(data)  # Comment to hide what Telegram is sending you
+    chat_id = data['message']['chat']['id']
+    message = data['message']['text']
+
+    json_data = {
+        "chat_id": chat_id,
+        "text": message,
+    }
+
+    message_url = BOT_URL + 'sendMessage'
+    requests.post(message_url, json=json_data)
+
+    return ''
+
+
+if __name__ == '__main__':  
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
