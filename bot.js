9b1aeb98d0825939631a6ec1b2a2ba3f43b83d0e
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-09 19:17:08 +0200

    env

--- bot.js
+++ bot.js
@@ -1,10 +1,22 @@
-var  env = process.env.BOT_KEY;
-var  token = '${env}';
+var env = process.env.BOT_KEY;
+var token = '${env}';
 
-var  Bot = require('node-telegram-bot-api'),
-  bot = new Bot(token, { polling: true });
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(env, {
+        polling: true
+    });
 
-bot.on('message', (msg) => {    
-    bot.sendMessage(msg.chat.id,"Hola chapu "+ env);        
+bot.on('message', (msg) => {
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+        bot.sendMessage(msg.chat.id, "Hello dear user");
+    }
+    var bye = "bye";
+    if (msg.text.toString().toLowerCase().includes(bye)) {
+        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+    }
+    var robot = "I'm robot";
+    if (msg.text.indexOf(robot) === 0) {
+        bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+    }
 });
-    
