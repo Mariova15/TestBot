0042dc6026a56b8c1a7d0f43d01697040feeb18f
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-09 20:07:57 +0200

    heroku

--- bot.js
+++ bot.js
@@ -1,6 +1,16 @@
-process.env.NTBA_FIX_319 = 1
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_KEY;
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const bot = new TelegramBot(token, {
     polling: true
