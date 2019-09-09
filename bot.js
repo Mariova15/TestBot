63f9f564ac063623eb58ed65d1588c7447d81ded
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-09 19:08:20 +0200

    Test

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const  env = process.env.BOT_KEY;
-const  token = '<${env}>';
+var  env = process.env.BOT_KEY;
+var  token = '<${env}>';
 
-const  Bot = require('node-telegram-bot-api'),
-const  bot = new Bot(token, { polling: true });
+var  Bot = require('node-telegram-bot-api'),
+  bot = new Bot(token, { polling: true });
 
 bot.on('message', (msg) => {    
     bot.sendMessage(msg.chat.id,"Hola chapu "+ env);        
