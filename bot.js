544ff164327cba8fde6e5debf0f6a5914b8c3481
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-09 19:50:45 +0200

    Arrow

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+process.env.NTBA_FIX_319 = 1
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_KEY;
 
@@ -5,13 +6,7 @@ const bot = new TelegramBot(token, {
     polling: true
 });
 
-const https = require('https')
-
-https.createServer().listen(process.env.PORT || 5000).on('request', function (req, res) {
-    Res.end()
-});
-
-bot.on('message', (msg) => {
+bot.on('message', function (msg) {
 
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
