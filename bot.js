7a338d8f2272177d565a3f19ea5c9d0be01a169f
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-09 20:29:19 +0200

    text

--- bot.js
+++ bot.js
@@ -6,11 +6,11 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const bot = new TelegramBot(token, {
     polling: true
@@ -18,9 +18,18 @@ const bot = new TelegramBot(token, {
 
 bot.on('message', function (msg) {
 
-    var Hi = "hi";
-    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(msg.chat.id, "Hello dear user");
-    }
+    
+
+    switch (Math.round(Math.random() * 3)) {
+        case 0:
+            bot.sendMessage(msg.chat.id, "Piano piano si arriva lontano");
+            break;
+        case 1:
+            bot.sendMessage(msg.chat.id, "Yo trabaje en IBM y te digo que esto funciona");
+            break;
+        case 2:
+            bot.sendMessage(msg.chat.id, "Bien, tienes nivel");
+            break;
+      }
 
 });
