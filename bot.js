afc7ba0b9345f0f31e6e2e5d6f92203d83b23130
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-09-09 19:28:46 +0200

    TestApi

--- bot.js
+++ bot.js
@@ -1,22 +1,15 @@
-var env = process.env.BOT_KEY;
-var token = '${env}';
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_KEY;
 
-var Bot = require('node-telegram-bot-api'),
-    bot = new Bot(env, {
-        polling: true
-    });
+const bot = new TelegramBot(token, {
+    polling: true
+});
 
 bot.on('message', (msg) => {
+
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
         bot.sendMessage(msg.chat.id, "Hello dear user");
     }
-    var bye = "bye";
-    if (msg.text.toString().toLowerCase().includes(bye)) {
-        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-    }
-    var robot = "I'm robot";
-    if (msg.text.indexOf(robot) === 0) {
-        bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
-    }
+
 });
